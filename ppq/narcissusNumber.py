439c57e728c55f6098dd20e9cea594eb32528ea8
yangshuxi <122294322@qq.com>
2017-06-16 16:49:18 +0800

    兔子

--- ppq/narcissusNumber.py
+++ ppq/narcissusNumber.py
@@ -1,8 +1,10 @@
 # 水仙花数
 def isNarcissus(num):
+    # 分别计算百位十位个位数字
     bw = int(num / 100)
     sw = int((num % 100) / 10)
     gw = num % 10
+    # 求和
     sum = (bw * bw * bw) + (sw * sw * sw) + (gw * gw * gw)
     if sum == num:
         return True
